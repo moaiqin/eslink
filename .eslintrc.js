ebd6641a745c6dc0a58f2f8ca45993ede95cf7d1
莫绍补 <shaobu.mo@weimob.com>
2019-01-10 22:56:26 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,13 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    // "extends": "eslint:recommended",//适用推荐的
     "parserOptions": {
         "ecmaVersion": 2015,
         "sourceType": "module"
     },
     "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
         "no-alert": 2,
         "no-undef": 2,
-        "no-console": 0
     }
 };
